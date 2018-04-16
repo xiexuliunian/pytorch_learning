bb338432d4b5d88aeee7338f1c375a560939855b
zxd <zzuzxd@126.com>
2018-04-16 10:51:55 +0800

    线性回归

---
+++ 3.线性回归.py
@@ -0,0 +1,47 @@
+# 1.导入必要包
+#%%
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.init as init
+from torch.autograd import Variable
+torch.manual_seed(11)#设置随机种子
+# 2.生产数据
+#%%
+num_data = 1000
+num_epoch = 1000
+
+noise = init.normal(torch.FloatTensor(num_data, 1), std=0.2)
+x = init.uniform(torch.Tensor(num_data, 1), -10, 10)
+y = 2 * x + 3
+y_noise = 2 * (x + noise) + 3
+# print(x,y,y_noise)
+
+# 3. 模型和优化
+model = nn.Linear(1, 1)
+output = model(Variable(x))
+
+loss_func = nn.L1Loss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# 4.训练
+loss_arr = []
+label = Variable(y_noise)
+for i in range(num_epoch):
+    output = model(Variable(x))
+    optimizer.zero_grad()
+
+    loss = loss_func(output, label)
+    loss.backward()
+    optimizer.step()
+    if i % 10 == 0:
+        print(loss)
+        loss_arr.append(loss.data.numpy()[0])
+
+
+param_list=list(model.parameters())
+# print(param_list[0].data,param_list[1].data)
+# print(param_list[0],param_list[1])
+print(param_list)
+#以上三种都可以读取模型参数
